4d4ca63ac788d63aa369d065b994022907cf81bd
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-09-21 00:16:26 +0300

    first

--- src/Task4/AssessmentJournal.java
+++ src/Task4/AssessmentJournal.java
@@ -3,76 +3,42 @@ package Task4;
 
 
 import Task2.*;
-import Task3.service.AlphabeticalOrderOfStudents;
-import Task3.service.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AssessmentJournal {
     private Map<Student, Map<Subject, Mark>> marksStudents;
+
+
+    public Map<Student, Map<Subject, Mark>> getMarksStudents() {
+        return marksStudents;
+    }
 
     public void setMarksStudents(final Map<Student, Map<Subject, Mark>> marksStudents) {
         this.marksStudents = marksStudents;
     }
 
-    public AssessmentJournal(final Map<Student, Map<Subject, Mark>> marksStudent) {
+    public AssessmentJournal(final Map<Student, Map<Subject, Mark>> marksStudents) {
         this.marksStudents = marksStudents;
     }
 
-    //всі оцінки одного студента
-    public Map<Subject, Mark> getStudentMark(final Student name) {
-        return marksStudents.get(name);
+    public Map<Subject, Mark> getMarkByStudent(final Student student) {
+        return marksStudents.get(student);
     }
-//    public static Map<Student, Map<Subject, Mark>> getMarksByStudent(final University university, final Student nameStudent) {
-//        final AssessmentJournal assessmentJournal = new AssessmentJournal();
-//        return assessmentJournal.getMarksStudents();
-//    }
 
-    //Зміна оцінки
-    public void setMarkSubject(final Student student, final Subject subject ,final Mark mark) {
+    public Map<Student, Mark> getMarkBySubject(final Subject subject) {
+        final Map<Student, Mark> mapMarkByStudent = new HashMap<Student, Mark>();
+
+        final Set<Student> set = marksStudents.keySet().stream().filter(e -> e.getSubjects().contains(subject)).collect(Collectors.toSet());
+        set.forEach(e -> mapMarkByStudent.put(e, marksStudents.get(e).get(subject)));
+        return  mapMarkByStudent;
+    }
+
+    public void changeMark(final Student student, final Subject subject ,final Mark mark) {
         marksStudents.get(student).put(subject,mark);
-    }
-
-
-//    public AssessmentJournal(final University university) {
-//        this.marksStudents = new HashMap<Student, Map<Subject, Mark>>();
-//        final AlphabeticalOrderOfStudents sortStudent = new AlphabeticalOrderOfStudents(university);
-//        final List<Student> listStudents = sortStudent();
-//
-//        for (final Student student : listStudents) {
-//            final Map<Subject, Mark> marksSubjects = new HashMap<Subject, Mark>();
-//
-//        }
-//    }
-
-
-//    public void changeMark(final String nameStudent, final Mark mark) {
-//        final Service service = new Service(university);
-//        service.getStudentsByName(nameStudent).setMark(mark);
-//    }
-
-//    public Mark getMarksByStudent(final University university, final String nameStudent) {
-//        final AssessmentJournal assessmentJournal = new AssessmentJournal(nameStudent, university);
-//        return assessmentJournal.getMarks();
-//    }
-
-    public static List<Integer> getMarksBySubject(final University university, final String nameSubject) {
-        final List<Integer> marks = new ArrayList<>();
-        for (final Group group : university.getGroups()) {
-            for (final Subject subject : group.getSubject()) {
-                if (subject.getName().equals(nameSubject)) {
-                    marks.addAll(subject.getMark().values());
-                }
-            }
-        }
-        return marks;
-    }
-}
-    public Map<Student, Map<Subject, Mark>> getMarksStudents() {
-        return marksStudents;
     }
 
     @Override
--- src/Task4/Task4.java
+++ src/Task4/Task4.java
@@ -3,6 +3,7 @@ package Task4;
 import Task2.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,10 +17,41 @@ public class Task4 {
         final Subject subject1 = new Subject("Higher Mathematics", date1);
         final Subject subject2 = new Subject("Algorithmization", date2);
         final Subject subject3 = new Subject("History", date3);
+        final Student student1 = new Student(Stream.of(
+                subject1, subject2, subject3).collect(Collectors.toList()),"Hvozdyk Sviatoslav-Valentyn Ruslanovych",
+                new Mark(5));
+        final Student student2 = new Student(Stream.of(
+                subject1, subject2, subject3).collect(Collectors.toList()),"Hamaiunov Oleksandr",
+        new Mark(5));
+        final  Student student3 = new Student(Stream.of(
+                subject1, subject2, subject3).collect(Collectors.toList()),"Khrapchun Roman Sergiyovuch",
+                new Mark(5));
         final AssessmentJournal journal = new AssessmentJournal(
-                Map.of(new Student(Stream.of(
-                        subject1, subject2, subject3).collect(Collectors.toList()),"Hvozdyk Sviatoslav-Valentyn Ruslanovych",
-                                new Mark(5)), Map.of(subject1, new Mark(4))));
+        new HashMap<>(){{
+            put(student1, new HashMap<>(){{
+                put(subject1, new Mark(4));
+                put(subject2, new Mark(5));
+                put(subject3, new Mark(5));
+            }});
+            put(student2, new HashMap<>(){{
+                put(subject1, new Mark(5));
+                put(subject2, new Mark(5));
+                put(subject3, new Mark(5));
+            }});
+            put(student3, new HashMap<>(){{
+                put(subject1, new Mark(3));
+                put(subject2, new Mark(4));
+                put(subject3, new Mark(3));
+            }});
+        }});
+
         System.out.println(journal);
+        System.out.println(" ");
+        journal.changeMark(student1,subject1, new Mark(5));
+        System.out.println(journal);
+        System.out.println(" ");
+        journal.getMarkByStudent(student1).forEach((subject, mark) -> System.out.println(student1.getFullName() + " " + subject.getName() + " " + mark));
+        System.out.println(" ");
+        journal.getMarkBySubject(subject2).forEach(((student, mark) -> System.out.println(student.getFullName() + " " + subject2.getName() + " " + mark)));
     }
 }
